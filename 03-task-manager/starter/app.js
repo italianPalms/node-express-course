fcb1cb90ace27448fd25b74ecc79eef67205ac7d
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-06 21:41:14 +0200

    connect port 3000 to app.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+
+
+const express = required('express');
+const app = express();
+
+
+// routes
+app.get('/hello', (req, res) => {
+res.send('Task Manager App')
+})
+
+const port = 3000
+
+app.listen(port, console.log('server s listening on port ${port}...'))
